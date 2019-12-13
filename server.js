19592282a70bdcf6f8154386b4943c4befcfd39f
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-12-13 11:58:24 -0500

    added routers

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const ApiRouter = require("./api/api-router.js");
-
 const server = express();
 server.use(helmet());
 server.use(express.json());
-server.use("/api/api", ApiRouter);
 
 module.exports = server;
