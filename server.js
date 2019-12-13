94efecf44cca0c41e453017536f7aa821e48868e
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-12-13 12:46:45 -0500

    added router and helpers

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const helmet = require("helmet");
 
+const projectRouter = require("./routers/projectRouter.js");
+const resourceRouter = require("./routers/resourceRouter.js");
+
 const server = express();
+
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourceRouter);
 
 module.exports = server;
