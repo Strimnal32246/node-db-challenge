87c9c61c34b02057283be5df3195ad879be9e112
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-12-13 11:34:22 -0500

    added knex and sever

---
+++ recipes/recipe-router.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const Schemes = require("./recipe-model.js");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log("recipe-router");
+  next();
+});
+
+module.exports = router;
